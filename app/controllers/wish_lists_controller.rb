3d4ee17ab04e3c55bac674972c6a74dcf8d33ffb
Eddie <eddie@5xcampus.com>
2022-11-17 15:37:49 +0800

    finish CRUD

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,4 +1,5 @@
 class WishListsController < ApplicationController
+  before_action :find_wish_list, only: [:edit, :update, :destroy, :show]
 
   def index
     @wish_lists = WishList.all
@@ -9,10 +10,7 @@ class WishListsController < ApplicationController
   end
 
   def create
-    # Strong Parameters
-    clean_params = params.require(:wish_list).permit(:title, :description)
-
-    @wish_list = WishList.new(clean_params)
+    @wish_list = WishList.new(wish_list_params)
 
     if @wish_list.save
       redirect_to root_path, notice: "成功新增許願卡！"
@@ -22,19 +20,13 @@ class WishListsController < ApplicationController
   end
 
   def show
-    @wish_list = WishList.find(params[:id])
   end
 
   def edit
-    @wish_list = WishList.find(params[:id])
   end
 
   def update
-    @wish_list = WishList.find(params[:id])
-
-    clean_params = params.require(:wish_list).permit(:title, :description)
-
-    if @wish_list.update(clean_params)
+    if @wish_list.update(wish_list_params)
       redirect_to root_path, notice: "成功更新許願卡！"
     else
       render :edit
@@ -42,8 +34,16 @@ class WishListsController < ApplicationController
   end
 
   def destroy
-    @wish_list = WishList.find(params[:id])
     @wish_list.destroy
     redirect_to root_path, notice: "資料已刪除"
+  end
+
+  private
+  def find_wish_list
+    @wish_list = WishList.find(params[:id])
+  end
+
+  def wish_list_params
+    params.require(:wish_list).permit(:title, :description)
   end
 end
