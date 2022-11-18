fbc9d88cd116f3fbd29fd25f32ff0a1b79b52761
Eddie <eddie@5xcampus.com>
2022-11-18 12:25:02 +0800

    encrypt user password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    render html: user_params
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: "註冊成功"
+    else
+      render :new
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   validates :password,
              confirmation: true,
              length: { minimum: 6 }
+
+  before_create :encrypt_password
+
+  private
+  def encrypt_password
+    self.password = Digest::SHA1.hexdigest("xy#{self.password.reverse}zz")
+  end
 end
